14594f5e92a3cc834647c609fb7cd348effe116f
Wilmer Oñate <Wilmer Oate@Wilmer>
2017-03-20 14:06:52 -0500

    Merge origin/master
    Conflicts:
    	src/BusinessLayer/UsuariosBL.java

--- src/BusinessLayer/UsuariosBL.java
+++ src/BusinessLayer/UsuariosBL.java
@@ -29,8 +29,8 @@ public class UsuariosBL {
         if (mensaje == null) {
             String password = usuarioDAL.recuperarContraseña(usuario);
             if (password != null) {
-                //if (password.equals(encriptar.encriptar(clave))) {
-                if (password.equals((clave))) {
+                if (password.equals(encriptar.encriptar(clave))) {
+                //if (password.equals((clave))) {
                     return null;
                 }
                 return "Contraseña incorrecta";
